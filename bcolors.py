760c2b8bb3579feedb70a6685ae91ca6af81e77f
Matt Mendez <xbtchunky@gmail.com>
2021-09-29 14:02:24 -0700

    Fix

--- bcolors.py
+++ bcolors.py
@@ -2,27 +2,27 @@ def wrap(s):
 	return '\033[' + s + '95m'
 
 class bcolors:
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    CYAN = '\033[96m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    DARK_GREEN = wrap('32')
-    DARK_RED = wrap('31')
-    DARK_BLUE = wrap('34')
-    DARK_PINK = wrap('35')
-    DARK_GREY = wrap('36')
-    RED_HIGHLIGHT = wrap('41')
-    GREEN_HIGHLIGHT = wrap('42')
-   	BLUE_HIGHLIGHT = wrap('44')
-   	PINK_HIGHLIGHT = wrap('45')
-   	GREY_HIGHLIGHT = wrap('46')
-   	YELLOW = wrap('93')
-   	PINK = wrap('95')
+	HEADER = '\033[95m'
+	BLUE = '\033[94m'
+	CYAN = '\033[96m'
+	GREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	DARK_GREEN = wrap('32')
+	DARK_RED = wrap('31')
+	DARK_BLUE = wrap('34')
+	DARK_PINK = wrap('35')
+	DARK_GREY = wrap('36')
+	RED_HIGHLIGHT = wrap('41')
+	GREEN_HIGHLIGHT = wrap('42')
+	BLUE_HIGHLIGHT = wrap('44')
+	PINK_HIGHLIGHT = wrap('45')
+	GREY_HIGHLIGHT = wrap('46')
+	YELLOW = wrap('93')
+	PINK = wrap('95')
 	HIGHLIGHT = wrap('7')
 	LIGHT_RED_HIGHLIGHT = wrap('101')
 	LIGHT_GREEN_HIGHLIGHT = wrap('102')
@@ -33,45 +33,45 @@ class bcolors:
 	WHITE_HIGHLIGHT = wrap('107')
 
 def dark_green_text(s):
-        return bcolors.DARK_GREEN + str(s) + bcolors.ENDC
+	return bcolors.DARK_GREEN + str(s) + bcolors.ENDC
 def dark_red_text(s):
-        return bcolors.DARK_RED + str(s) + bcolors.ENDC
+	return bcolors.DARK_RED + str(s) + bcolors.ENDC
 def dark_blue_text(s):
-        return bcolors.DARK_BLUE + str(s) + bcolors.ENDC
+	return bcolors.DARK_BLUE + str(s) + bcolors.ENDC
 def dark_pink_text(s):
-        return bcolors.DARK_PINK + str(s) + bcolors.ENDC
+	return bcolors.DARK_PINK + str(s) + bcolors.ENDC
 def dark_grey_text(s):
-        return bcolors.DARK_GREY + str(s) + bcolors.ENDC
+	return bcolors.DARK_GREY + str(s) + bcolors.ENDC
 def red_highlight_text(s):
-        return bcolors.RED_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.RED_HIGHLIGHT + str(s) + bcolors.ENDC
 def green_highlight_text(s):
-        return bcolors.GREEN_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.GREEN_HIGHLIGHT + str(s) + bcolors.ENDC
 def blue_highlight_text(s):
-        return bcolors.BLUE_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.BLUE_HIGHLIGHT + str(s) + bcolors.ENDC
 def pink_highlight_text(s):
-        return bcolors.PINK_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.PINK_HIGHLIGHT + str(s) + bcolors.ENDC
 def grey_highlight_text(s):
-        return bcolors.GREY_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.GREY_HIGHLIGHT + str(s) + bcolors.ENDC
 def yellow_text(s):
-        return bcolors.YELLOW + str(s) + bcolors.ENDC
+	return bcolors.YELLOW + str(s) + bcolors.ENDC
 def pink_text(s):
-        return bcolors.PINK + str(s) + bcolors.ENDC
+	return bcolors.PINK + str(s) + bcolors.ENDC
 def highlight_text(s):
-        return bcolors.HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.HIGHLIGHT + str(s) + bcolors.ENDC
 def light_red_highlight_text(s):
-        return bcolors.LIGHT_RED_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.LIGHT_RED_HIGHLIGHT + str(s) + bcolors.ENDC
 def light_green_highlight_text(s):
-        return bcolors.LIGHT_GREEN_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.LIGHT_GREEN_HIGHLIGHT + str(s) + bcolors.ENDC
 def light_yellow_highlight_text(s):
-        return bcolors.LIGHT_YELLOW_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.LIGHT_YELLOW_HIGHLIGHT + str(s) + bcolors.ENDC
 def light_blue_highlight_text(s):
-        return bcolors.LIGHT_BLUE_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.LIGHT_BLUE_HIGHLIGHT + str(s) + bcolors.ENDC
 def light_pink_highlight_text(s):
-        return bcolors.LIGHT_PINK_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.LIGHT_PINK_HIGHLIGHT + str(s) + bcolors.ENDC
 def cyan_highlight_text(s):
-        return bcolors.CYAN_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.CYAN_HIGHLIGHT + str(s) + bcolors.ENDC
 def white_highlight_text(s):
-        return bcolors.WHITE_HIGHLIGHT + str(s) + bcolors.ENDC
+	return bcolors.WHITE_HIGHLIGHT + str(s) + bcolors.ENDC
 def blue_text(s):
 	return bcolors.BLUE + str(s) + bcolors.ENDC
 def cyan_text(s):
@@ -84,7 +84,7 @@ def underlined_text(s):
 	return bcolors.UNDERLINE + str(s) + bcolors.ENDC
 
 def greenprint(s):
-    print(bcolors.UNDERLINE + bcolors.GREEN + str(s) + bcolors.ENDC)
+	print(bcolors.UNDERLINE + bcolors.GREEN + str(s) + bcolors.ENDC)
 
 def failprint(s):
 	print(bcolors.FAIL + bcolors.UNDERLINE + str(s) + bcolors.ENDC)
